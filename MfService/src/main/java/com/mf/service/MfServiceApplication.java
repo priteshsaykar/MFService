28edc8632aafac59577673c910c82179b02acf8b
admin <admin@DESKTOP-8K41KKH>
2025-04-05 18:41:34 +0530

    first cmmt

---
+++ MfService/src/main/java/com/mf/service/MfServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mf.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfServiceApplication.class, args);
+	}
+
+}
---
+++ MfService/src/test/java/com/mf/service/MfServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mf.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
